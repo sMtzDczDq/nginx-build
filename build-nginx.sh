c09e2121355c7bcd143951e47e015bbcfe30c022
sMtzDczDq <75165692+sMtzDczDq@users.noreply.github.com>
2024-12-14 23:39:42 +0100

    Update build-nginx.sh

--- build-nginx.sh
+++ build-nginx.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Run as root or with sudo
 if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root or with sudo."
-  exit 1
+	echo "This script must be run as root or with sudo."
+	exit 1
 fi
 
 # Make script exit if a simple command fails and
@@ -10,9 +10,15 @@ fi
 set -e -x
 
 # Ensure curl is installed
-apt-get update && apt-get install curl -y
+apt-get update && apt-get install curl jq -y
+
+GH_API_HEADERS='-H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"'
 
 # Set URLs to the source directories
+REPO_PCRE="PCRE2Project/pcre2"
+PCRE_TAR=$(
+	curl --silent "$GH_API_HEADERS" --url https://api.github.com/repos/"$REPO_PCRE"/releases/latest --output - | jq '.assets[]|select(.content_type == "application/gzip")|.browser_download_url'
+)
 source_pcre=https://onboardcloud.dl.sourceforge.net/project/pcre/pcre/8.45/
 source_zlib=https://zlib.net/
 source_openssl=https://www.openssl.org/source/
@@ -42,20 +48,21 @@ today=$(date +"%Y-%m-%d")
 
 # Clean out any files from previous runs of this script
 rm -rf \
-  "$bpath" \
-  /etc/nginx-default
+	"$bpath" \
+	/etc/nginx-default
 mkdir "$bpath"
 
 # Ensure the required software to compile NGINX is installed
 apt-get -y install \
-  binutils \
-  build-essential \
-  curl \
-  dirmngr \
-  libssl-dev
+	binutils \
+	build-essential \
+	curl \
+	dirmngr \
+	libssl-dev
 
 # Download the source files
-curl -L "${source_pcre}${version_pcre}.tar.gz" -o "${bpath}/pcre.tar.gz"
+#curl -L "${source_pcre}${version_pcre}.tar.gz" -o "${bpath}/pcre.tar.gz"
+curl -L "$PCRE_TAR" -o "${bpath}/pcre.tar.gz"
 curl -L "${source_zlib}${version_zlib}.tar.gz" -o "${bpath}/zlib.tar.gz"
 curl -L "${source_openssl}${version_openssl}.tar.gz" -o "${bpath}/openssl.tar.gz"
 curl -L "${source_nginx}${version_nginx}.tar.gz" -o "${bpath}/nginx.tar.gz"
@@ -79,27 +86,27 @@ gpg --batch --verify nginx.tar.gz.asc nginx.tar.gz
 # Expand the source files
 cd "$bpath"
 for archive in ./*.tar.gz; do
-  tar xzf "$archive"
+	tar xzf "$archive"
 done
 
 # Clean up source files
 rm -rf \
-  "$GNUPGHOME" \
-  "$bpath"/*.tar.*
+	"$GNUPGHOME" \
+	"$bpath"/*.tar.*
 
 # Rename the existing /etc/nginx directory so it's saved as a back-up
 if [ -d "/etc/nginx" ]; then
-  mv /etc/nginx "/etc/nginx-${today}"
+	mv /etc/nginx "/etc/nginx-${today}"
 fi
 
 # Create NGINX cache directories if they do not already exist
 if [ ! -d "/var/cache/nginx/" ]; then
-  mkdir -p \
-    /var/cache/nginx/client_temp \
-    /var/cache/nginx/proxy_temp \
-    /var/cache/nginx/fastcgi_temp \
-    /var/cache/nginx/uwsgi_temp \
-    /var/cache/nginx/scgi_temp
+	mkdir -p \
+		/var/cache/nginx/client_temp \
+		/var/cache/nginx/proxy_temp \
+		/var/cache/nginx/fastcgi_temp \
+		/var/cache/nginx/uwsgi_temp \
+		/var/cache/nginx/scgi_temp
 fi
 
 # Add NGINX group and user if they do not already exist
@@ -107,79 +114,78 @@ id -g nginx &>/dev/null || addgroup --system nginx
 id -u nginx &>/dev/null || adduser --disabled-password --system --home /var/cache/nginx --shell /sbin/nologin --group nginx
 
 # Test to see if our version of gcc supports __SIZEOF_INT128__
-if gcc -dM -E - </dev/null | grep -q __SIZEOF_INT128__
-then
-  ecflag="enable-ec_nistp_64_gcc_128"
+if gcc -dM -E - </dev/null | grep -q __SIZEOF_INT128__; then
+	ecflag="enable-ec_nistp_64_gcc_128"
 else
-  ecflag=""
+	ecflag=""
 fi
 
 # Build NGINX, with various modules included/excluded
 cd "$bpath/$version_nginx"
 ./configure \
-  --prefix=/etc/nginx \
-  --with-cc-opt="-O3 -fPIE -fstack-protector-strong -Wformat -Werror=format-security" \
-  --with-ld-opt="-Wl,-Bsymbolic-functions -Wl,-z,relro" \
-  --with-pcre="$bpath/$version_pcre" \
-  --with-zlib="$bpath/$version_zlib" \
-  --with-openssl-opt="no-weak-ssl-ciphers no-ssl3 no-shared $ecflag -DOPENSSL_NO_HEARTBEATS -fstack-protector-strong" \
-  --with-openssl="$bpath/$version_openssl" \
-  --sbin-path=/usr/sbin/nginx \
-  --modules-path=/usr/lib/nginx/modules \
-  --conf-path=/etc/nginx/nginx.conf \
-  --error-log-path=/var/log/nginx/error.log \
-  --http-log-path=/var/log/nginx/access.log \
-  --pid-path=/var/run/nginx.pid \
-  --lock-path=/var/run/nginx.lock \
-  --http-client-body-temp-path=/var/cache/nginx/client_temp \
-  --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
-  --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
-  --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
-  --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
-  --user=nginx \
-  --group=nginx \
-  --with-file-aio \
-  --with-http_auth_request_module \
-  --with-http_gunzip_module \
-  --with-http_gzip_static_module \
-  --with-http_mp4_module \
-  --with-http_realip_module \
-  --with-http_secure_link_module \
-  --with-http_slice_module \
-  --with-http_ssl_module \
-  --with-http_stub_status_module \
-  --with-http_sub_module \
-  --with-http_v2_module \
-  --with-pcre-jit \
-  --with-stream \
-  --with-stream_ssl_module \
-  --with-threads \
-  --without-http_empty_gif_module \
-  --without-http_geo_module \
-  --without-http_split_clients_module \
-  --without-http_ssi_module \
-  --without-mail_imap_module \
-  --without-mail_pop3_module \
-  --without-mail_smtp_module
+	--prefix=/etc/nginx \
+	--with-cc-opt="-O3 -fPIE -fstack-protector-strong -Wformat -Werror=format-security" \
+	--with-ld-opt="-Wl,-Bsymbolic-functions -Wl,-z,relro" \
+	--with-pcre="$bpath/$version_pcre" \
+	--with-zlib="$bpath/$version_zlib" \
+	--with-openssl-opt="no-weak-ssl-ciphers no-ssl3 no-shared $ecflag -DOPENSSL_NO_HEARTBEATS -fstack-protector-strong" \
+	--with-openssl="$bpath/$version_openssl" \
+	--sbin-path=/usr/sbin/nginx \
+	--modules-path=/usr/lib/nginx/modules \
+	--conf-path=/etc/nginx/nginx.conf \
+	--error-log-path=/var/log/nginx/error.log \
+	--http-log-path=/var/log/nginx/access.log \
+	--pid-path=/var/run/nginx.pid \
+	--lock-path=/var/run/nginx.lock \
+	--http-client-body-temp-path=/var/cache/nginx/client_temp \
+	--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+	--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+	--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+	--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+	--user=nginx \
+	--group=nginx \
+	--with-file-aio \
+	--with-http_auth_request_module \
+	--with-http_gunzip_module \
+	--with-http_gzip_static_module \
+	--with-http_mp4_module \
+	--with-http_realip_module \
+	--with-http_secure_link_module \
+	--with-http_slice_module \
+	--with-http_ssl_module \
+	--with-http_stub_status_module \
+	--with-http_sub_module \
+	--with-http_v2_module \
+	--with-pcre-jit \
+	--with-stream \
+	--with-stream_ssl_module \
+	--with-threads \
+	--without-http_empty_gif_module \
+	--without-http_geo_module \
+	--without-http_split_clients_module \
+	--without-http_ssi_module \
+	--without-mail_imap_module \
+	--without-mail_pop3_module \
+	--without-mail_smtp_module
 make
 make install
 make clean
 strip -s /usr/sbin/nginx*
 
 if [ -d "/etc/nginx-${today}" ]; then
-  # Rename the default /etc/nginx settings directory so it's accessible as a reference to the new NGINX defaults
-  mv /etc/nginx /etc/nginx-default
+	# Rename the default /etc/nginx settings directory so it's accessible as a reference to the new NGINX defaults
+	mv /etc/nginx /etc/nginx-default
 
-  # Restore the previous version of /etc/nginx to /etc/nginx so the old settings are kept
-  mv "/etc/nginx-${today}" /etc/nginx
+	# Restore the previous version of /etc/nginx to /etc/nginx so the old settings are kept
+	mv "/etc/nginx-${today}" /etc/nginx
 fi
 
 # Create NGINX systemd service file if it does not already exist
 if [ ! -e "/lib/systemd/system/nginx.service" ]; then
-  # Control will enter here if the NGINX service doesn't exist.
-  file="/lib/systemd/system/nginx.service"
+	# Control will enter here if the NGINX service doesn't exist.
+	file="/lib/systemd/system/nginx.service"
 
-  /bin/cat >$file <<'EOF'
+	/bin/cat >$file <<'EOF'
 [Unit]
 Description=The NGINX HTTP and reverse proxy server
 After=syslog.target network.target remote-fs.target nss-lookup.target
@@ -198,6 +204,6 @@ WantedBy=multi-user.target
 EOF
 fi
 
-echo "All done.";
+echo "All done."
 echo "Start with sudo systemctl start nginx"
 echo "or with sudo nginx"
